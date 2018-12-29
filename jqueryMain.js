efb3b599d6ece730cb789afafd63cd10c1742afa
Upasana Sinha <42410636+Usinha31@users.noreply.github.com>
2018-12-29 17:15:58 +0530

    Add files via upload

---
+++ jqueryMain.js
@@ -0,0 +1,49 @@
+
+function isEmail(email) {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+  }
+
+  $("#getStarted").click(function() {
+
+              var errorMessage = "";
+              var fieldsMissing = "";
+
+              if ($("#email").val() == "") {
+
+                  fieldsMissing += "<br>Email";
+              }
+
+
+               if (fieldsMissing != "") {
+
+                   errorMessage += "The following field(s) are missing:" + fieldsMissing;
+               }
+
+               if (isEmail($("#email").val()) == false) {
+
+                  errorMessage += "<p>Your email address is not valid</p>";
+
+              }
+
+              if (errorMessage != "") {
+
+                  $("#errorMessage").html(errorMessage);
+
+              } else {
+                  $('#myModal').modal('show');
+              }
+
+             $("#save").click(function() {
+                 if ($("#password").val() != "") {
+                  $("#myModal").modal('hide');
+                  var userName = $("#email").val();
+                  $("#signUp").html("Hi, " + userName + "!");
+                  $("#password").val("");
+                  $("#email").val("");
+                  e.preventDefault();
+              }
+
+             })
+
+            });
