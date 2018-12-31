ea1e1aa49a675c0aa88787e31ea5fe4e33480087
Upasana Sinha <usinha62@gmail.com>
2018-12-31 15:30:14 +0530

    Update jqueryMain.js

--- jqueryMain.js
+++ jqueryMain.js
@@ -47,3 +47,18 @@ function isEmail(email) {
             })
 
            });
+
+           function deleteLi(Li){
+            $(Li).parent().parent().remove()
+        }
+        function completeLi(Li){
+            $(Li).parent().parent().addClass("completed")
+        }
+        $('#submit').click(function(event){
+            $('#internalList').append($('<li class="upasana">').text($("#entryinput").val()))
+            $(`<div class="todo-btns">
+               <button class="complete-btn" onclick="completeLi($(this))">Complete</button>
+               <button class="delete-btn" onclick="deleteLi($(this))">Delete</button>
+            </div>`).appendTo(".upasana:last-child")
+            $('#entryinput').val('')
+        });
