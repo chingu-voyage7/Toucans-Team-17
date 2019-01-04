a54bf79440208e548afb3998e0e6ead358e12f47
Upasana Sinha <usinha62@gmail.com>
2019-01-04 21:43:50 +0530

    Update jqueryMain.js

--- jqueryMain.js
+++ jqueryMain.js
@@ -48,15 +48,12 @@ function isEmail(email) {
            function deleteLi(Li){
             $(Li).parent().parent().remove()
         }
-        function completeLi(Li){
-            $(Li).parent().parent().addClass("completed")
-        }
+
         $('#entryinput').keydown(function(e){
             if (e.which === 13) {
             e.preventDefault();
             $('#internalList').append($('<li class="upasana">').text($("#entryinput").val()))
             $(`<div class="todo-btns">
-               <button class="complete-btn" onclick="completeLi($(this))">Complete</button>
                <button class="delete-btn" onclick="deleteLi($(this))">Delete</button>
             </div>`).appendTo(".upasana:last-child")
             $('#entryinput').val('')
