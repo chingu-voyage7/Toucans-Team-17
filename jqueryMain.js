d1c48b3661d6d5df2f8cfd1034d2163c62058363
Upasana Sinha <usinha62@gmail.com>
2019-01-09 18:54:59 +0530

    Update jqueryMain.js

--- jqueryMain.js
+++ jqueryMain.js
@@ -1,81 +1,75 @@
-
 function isEmail(email) {
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        return regex.test(email);
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+$("#getStarted").click(function() {
+  var errorMessage = "";
+  var fieldsMissing = "";
+
+  if ($("#email").val() == "") {
+    fieldsMissing += "<br>Email";
   }
 
-  $("#getStarted").click(function() {
+  if ($("#password").val() == "") {
+    fieldsMissing += "<br>Password";
+  }
 
-              var errorMessage = "";
-              var fieldsMissing = "";
+  if (fieldsMissing != "") {
+    errorMessage += "The following field(s) are missing:" + fieldsMissing;
+  }
 
-              if ($("#email").val() == "") {
+  if (isEmail($("#email").val()) == false) {
+    errorMessage += "<p>Your email address is not valid</p>";
+  }
 
-                  fieldsMissing += "<br>Email";
-              }
+  if (errorMessage != "") {
+    $("#errorMessage").html(errorMessage);
+  } else {
+    var userName = $("#email").val();
+    $("#login").html("Hi, " + userName + "!");
+    $("#password").val("");
+    $("#email").val("");
+    $("#errorMessage").hide();
+    alert("Congratulations, you did it!");
+  }
+});
 
-              if ($("#password").val() == "") {
+function deleteLi(Li) {
+  $(Li)
+    .parent()
+    .parent()
+    .remove();
+}
 
-                  fieldsMissing += "<br>Password";
-              }
-
-               if (fieldsMissing != "") {
-
-                   errorMessage += "The following field(s) are missing:" + fieldsMissing;
-               }
-
-               if (isEmail($("#email").val()) == false) {
-
-                  errorMessage += "<p>Your email address is not valid</p>";
-
-              }
-
-              if (errorMessage != "") {
-
-                  $("#errorMessage").html(errorMessage);
-
-              } else {
-                var userName = $("#email").val();
-                $("#login").html("Hi, " + userName + "!");
-                $("#password").val("");
-                $("#email").val("");
-                $("#errorMessage").hide();
-                alert("Congratulations, you did it!");
-              }
-
-           });
-
-           function deleteLi(Li){
-            $(Li).parent().parent().remove()
-        }
-
-        $('#entryinput').keydown(function(e){
-            if (e.which === 13) {
-            e.preventDefault();
-            $('#internalList').append($('<li class="upasana">').text($("#entryinput").val()))
-            $(`<div class="todo-btns">
+$("#entryinput").keydown(function(e) {
+  if (e.which === 13) {
+    e.preventDefault();
+    $("#internalList").append(
+      $('<li class="upasana">').text($("#entryinput").val())
+    );
+    $(`<div class="todo-btns">
                <button class="delete-btn" onclick="deleteLi($(this))">Delete</button>
-            </div>`).appendTo(".upasana:last-child")
-            $('#entryinput').val('')
-          }
-        });
+            </div>`).appendTo(".upasana:last-child");
+    $("#entryinput").val("");
+  }
+});
 
-        $("#add").click(function() {
-          $("#largeModal").modal('show');
-        });
+$("#add").click(function() {
+  $("#largeModal").modal("show");
+});
 
-        $("#submit").click(function() {
-          if ($("#todo-list").val() == "") {
-            alert("Please enter your resolutions!");
-          } else {
-            $("#largeModal").modal('hide');
-            $("#myModal").modal('show');
-          }
-        });
+$("#submit").click(function() {
+  if ($("#internalList").has(".upasana").length === 0) {
+    alert("Please enter your resolutions!");
+  } else {
+    $("#largeModal").modal("hide");
+    $("#myModal").modal("show");
+  }
+});
 
-        $("#myModal a").click(function () {
-          $("#myModal").modal('hide');
-        });
-
+$("#myModal a").click(function() {
+  $("#myModal").modal("hide");
+});
 
 // $("#myModal").modal('hide');
